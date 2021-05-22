3c37165e00c4058e85af13099985b6b1559338bc
Chizzy-codes <chizurumolorondu@gmail.com>
2021-05-22 23:52:15 +0100

    Updating

--- superset_config.py
+++ superset_config.py
@@ -1,25 +1,16 @@
-import os
-# ---------------------------------------------------------
 # Superset specific config
-# ---------------------------------------------------------
-# ROW_LIMIT = 5000
-SUPERSET_WORKERS = 1  # for it to work in heroku basic/hobby dynos increase as you like
-SUPERSET_WEBSERVER_PORT = os.environ['PORT']
-# ---------------------------------------------------------
-MAPBOX_API_KEY = os.getenv('MAPBOX_API_KEY')
+ROW_LIMIT = 5000
 
-# ---------------------------------------------------------
 # Flask App Builder configuration
-# ---------------------------------------------------------
 # Your App secret key
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'lK-Ef8QDSHyQ_eCuPuS26h7EER19IRDfy9D0N5rcHtA='
 
 # The SQLAlchemy connection string to your database backend
 # This connection defines the path to the database that stores your
-# Superset metadata (slices, connections, tables, dashboards, ...).
+# superset metadata (slices, connections, tables, dashboards, ...).
 # Note that the connection information to connect to the datasources
 # you want to explore are managed directly in the web UI
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = 'postgres://mkksdclxfvuzqi:d48a32b37dea6621dc303a981d0cfac93c17cb52bc95fd57e27a8218b6e87032@ec2-18-215-111-67.compute-1.amazonaws.com:5432/d8cj90v5okhm5a'
 
 # Flask-WTF flag for CSRF
 WTF_CSRF_ENABLED = CSRF_ENABLED = True
